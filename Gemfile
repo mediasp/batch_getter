d051f705790a2ab21ed043c55e26f01bf351a17d
Tom Chipchase <tom@mediasp.com>
2014-11-05 12:04:47 +0000

    tidied things up a bit

--- Gemfile
+++ Gemfile
@@ -2,25 +2,9 @@
 
 source 'https://rubygems.org'
 
-# we need to install this with a version of bundler that doesn't know what this
-# means
-# ruby '1.9.3'
-
 gem 'rack'
-gem 'rest-client', '~> 1.6.8'
-
-# These aren't things we require, but we need to lock the versions due to the
-# nested gem dependencies.
-gem 'mime-types', '~> 1.25.1'
-gem 'rubyzip', '0.9.9'
+gem 'rest-client'
 
 platforms :mri do
   gem 'thin'
-end
-
-group :development do
-  gem 'foreman'
-  gem 'shotgun'
-  gem 'pkgr'
-  gem 'bump'
 end
--- config/warble.rb
+++ config/warble.rb
@@ -1,9 +1,11 @@
 Warbler::Config.new do |config|
-  config.features = %w(executable)
-  config.dirs = %w(config lib)
+  config.features = %w()
+  config.dirs = %w(lib)
+  # We need config.ru, and the Gemfiles to generate the war, but they don't
+  # actually need to be included inside it.
+  config.excludes = %w(Gemfile Gemfile.lock config.ru)
   config.bundle_without = %w(development)
   config.gem_dependencies = true
   config.gem_excludes = [/^(test|spec)\//]
-  config.webserver = 'jetty'
-  config.webxml.jruby.compat.version = "1.9"
+  config.webxml.jruby.compat.version = '2.0'
 end
