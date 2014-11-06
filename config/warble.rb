a12e1c5fe3aea9827aab49db46b523762699b1ea
Tom Chipchase <tom@mediasp.com>
2014-11-06 12:13:53 +0000

    suffix the version to the name of the war

--- config/warble.rb
+++ config/warble.rb
@@ -4,6 +4,8 @@ Warbler::Config.new do |config|
   # We need config.ru, and the Gemfiles to generate the war, but they don't
   # actually need to be included inside it.
   config.excludes = %w(Gemfile Gemfile.lock config.ru)
+  # Add the version to the name, so storing them is easier.
+  config.jar_name = "batch_getter-#{`cat VERSION`.strip}"
   config.bundle_without = %w(development)
   config.gem_dependencies = true
   config.gem_excludes = [/^(test|spec)\//]
