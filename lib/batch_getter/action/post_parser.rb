a6904a9b8b9575f6da7976b6653a8f2a8bc4e2da
Tom Chipchase <tom@mediasp.com>
2014-11-07 12:49:06 +0000

    added some error checking to post parser

--- lib/batch_getter/action/post_parser.rb
+++ lib/batch_getter/action/post_parser.rb
@@ -4,12 +4,18 @@ module BatchGetter
   module Action
     # Parses a post request and returns a list of URLs
     class PostParser
+      class Error < StandardError
+      end
+
       def initialize(base, data)
         @base = base
         @data = JSON.parse(data)
+      rescue
+        raise Error
       end
 
       def call
+        fail Error if @data.respond_to? :keys
         @data.map { |path| File.join(@base, path) }
       end
     end
--- test/batch_getter/action/post_parser_test.rb
+++ test/batch_getter/action/post_parser_test.rb
@@ -5,11 +5,11 @@ require 'batch_getter/action/post_parser'
 describe BatchGetter::Action::PostParser do
   let(:base_url) { 'www.example.com' }
 
+  subject { BatchGetter::Action::PostParser.new(base_url, json_array) }
+
   describe '#call' do
     describe 'given a JSON array' do
       let(:json_array) { %q([ "foo", "bar" ]) }
-
-      subject { BatchGetter::Action::PostParser.new(base_url, json_array) }
 
       it 'should return an array of URLs' do
         assert_equal %w(www.example.com/foo www.example.com/bar), subject.call
@@ -17,11 +17,19 @@ describe BatchGetter::Action::PostParser do
     end
 
     describe 'given JSON hash' do
-      it 'should raise an error'
+      let(:json_array) { %q({ "foo": "foo", "bar": "bar" }) }
+
+      it 'should raise an error' do
+        assert_raises(BatchGetter::Action::PostParser::Error) { subject.call }
+      end
     end
 
     describe 'given non-JSON data' do
-      it 'should raise an error'
+      let(:json_array) { %q(not really a json string) }
+
+      it 'should raise an error' do
+        assert_raises(BatchGetter::Action::PostParser::Error) { subject.call }
+      end
     end
   end
 end
