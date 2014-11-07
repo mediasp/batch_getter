debc2e5932c3837162b974e999204257bd1a9038
Tom Chipchase <tom@mediasp.com>
2014-11-07 17:30:48 +0000

    resources_getter doesn't have to know anything about cookies

--- lib/batch_getter/action/resources_getter.rb
+++ lib/batch_getter/action/resources_getter.rb
@@ -2,17 +2,13 @@ module BatchGetter
   module Action
     # Gets multiple resources.
     class ResourcesGetter
-      def initialize(cookie_jar, resource_getter, uris)
-        @cookie_jar = cookie_jar
+      def initialize(resource_getter, uris)
         @resource_getter = resource_getter
         @uris = uris
       end
 
       def call
-        @uris.map do |uri|
-          cookies = @cookie_jar.cookies
-          @resource_getter.call(uri, cookies)
-        end
+        @uris.map { |uri| @resource_getter.call(uri) }
       end
     end
   end
--- test/batch_getter/action/resources_getter_test.rb
+++ test/batch_getter/action/resources_getter_test.rb
@@ -4,14 +4,9 @@ require 'mocha/mini_test'
 
 describe BatchGetter::Action::ResourcesGetter do
   let(:resource_getter) { mock }
-  let(:cookie_jar) do
-    mock.tap do |cookie_jar|
-      cookie_jar.stubs(:cookies).returns([])
-    end
-  end
 
   subject do
-    BatchGetter::Action::ResourcesGetter.new(cookie_jar, resource_getter, uris)
+    BatchGetter::Action::ResourcesGetter.new(resource_getter, uris)
   end
 
   describe '#call' do
@@ -20,27 +15,12 @@ describe BatchGetter::Action::ResourcesGetter do
     let(:bar) { { 'bar' => 2 } }
 
     it 'returns the json from each uri in an array' do
-      resource_getter.expects(:call).with('foo', []).returns(foo)
-      resource_getter.expects(:call).with('bar', []).returns(bar)
+      resource_getter.expects(:call).with('foo').returns(foo)
+      resource_getter.expects(:call).with('bar').returns(bar)
       response = subject.call
 
       assert_same foo, response.first
       assert_same bar, response.last
-    end
-
-    describe 'first response returns set-cookie header' do
-      let(:cookie_jar) do
-        mock.tap do |cookie_jar|
-          cookie_jar.stubs(:cookies).returns([], ['foo'])
-        end
-      end
-
-      it 'sends cookie from previous request to next request' do
-        resource_getter.expects(:call).with('foo', []).returns(foo)
-        resource_getter.expects(:call).with('bar', ['foo']).returns(bar)
-
-        subject.call
-      end
     end
   end
 end
