47e344fce962ba8991dfc2f0d4b3986cf4532c06
Tom Chipchase <tom@mediasp.com>
2014-11-11 12:43:06 +0000

    batch getter split into several files

--- lib/batch_getter.rb
+++ lib/batch_getter.rb
@@ -3,10 +3,10 @@
 require 'json'
 require 'confuse'
 
-require 'action/post_parser'
-require 'action/resource_getter'
-require 'action/resources_getter'
-require 'action/response_creator'
+require 'batch_getter/action/post_parser'
+require 'batch_getter/action/resource_getter'
+require 'batch_getter/action/resources_getter'
+require 'batch_getter/action/response_creator'
 
 CONFIG_FILE = if ENV['RACK_ENV'] == 'production'
                 '/etc/batch_getter/config.ini'
@@ -16,55 +16,56 @@ CONFIG_FILE = if ENV['RACK_ENV'] == 'production'
 
 # Batch getter
 module BatchGetter
-  def config(location: CONFIG_FILE)
-    @config ||= Confuse.config path: location do |conf|
-      conf.add_item :api_endpoint, description: 'API to batch requests to.',
-                                   type: String
-
-      conf.add_item :strict_fail_codes,
-                    default: [], type: Array,
-                    description: 'Fail the whole request if one of these '\
-                    'error codes is received (otherwise, the '\
-                    'error message is included in the JSON data)'
-    end
-  end
-
   class << self
     def call(env)
-      handle_request(env)
-    rescue => error
-      [500, { 'Content-Type' => 'application/json' },
-       Array({ error: 500, message: error }.to_json)]
+      RequestHandler.new(config.api_endpoint, env).call
+      # rescue => error
+      #   [500, { 'Content-Type' => 'application/json' },
+      #    Array({ error: 500, message: error }.to_json)]
     end
 
     private
 
-    def post_parser
-      proc { |data| Action::PostParser.new(config.api_endpoint, data).call }
+    def config(location: CONFIG_FILE)
+      @config ||= Confuse.config path: location do |conf|
+        conf.add_item :api_endpoint, description: 'API to batch requests to.',
+                                     type: String
+
+        conf.add_item :strict_fail_codes,
+                      default: [], type: Array,
+                      description: 'Fail the whole request if one of these '\
+                      'error codes is received (otherwise, the '\
+                      'error message is included in the JSON data)'
+      end
+    end
+  end
+
+  # request handler
+  class RequestHandler
+    def initialize(api, env)
+      @request = Rack::Request.new(env)
+      @headers = env.select { |key, _value| /^HTTP_/.match(key) }
+      @rest_client = RestClient::Resource.new(api)
     end
 
-    def resource_getter(headers)
-      proc do |uri|
-        Action::ResourceGetter.new(headers, cookie_jar, uri,
-                                   strict_error_codes: config.strict_fail_codes)
-          .call
+    def call
+      paths = Action::PostParser.new(@request.body.read).call
+      resources_getter = Action::ResourcesGetter.new(resource_getter, paths)
+      body, cookies = resources_getter.call
+      cookies = parse_cookies(cookies)
+      Action::ResponseCreator.new(body, cookies).call
+    end
+
+    private
+
+    def resource_getter
+      @resource_getter ||= proc do |path|
+        Action::ResourceGetter.new(path, @headers, @rest_client).call
       end
     end
 
-    def resources_getter(headers, uris)
-      Action::ResourcesGetter.new(resource_getter(headers), uris)
-    end
-
-    def resonse_creator(body)
-      Action::ResponseCreator.new(cookie_jar, body)
-    end
-
-    def handle_request(env)
-      request = Rack::Request.new(env)
-      uris = post_parser.call(request.body.read)
-      resources_getter = resource_getter(request.headers, uris)
-      body = resources_getter.call
-      response_creator(body).call
+    def parse_cookies(cookies)
+      cookies.map { |cookie| cookie.join('=') }
     end
   end
 end
